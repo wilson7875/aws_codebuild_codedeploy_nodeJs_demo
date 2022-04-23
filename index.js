097c2e8d21e94ea9716fdb9c1b1a15f987f475b4
wilson <wilsonbrahmane1999@gmail.com>
2022-04-23 11:21:18 +0530

    v2 commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V1')
+  res.send('CICD App V2 for webhook')
 })
 
 app.get('/status', (req, res) => {
