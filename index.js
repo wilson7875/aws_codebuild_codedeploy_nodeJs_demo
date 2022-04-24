242204a9b88593e4dfb6a830f752b0c507fa1392
wilson <wilsonbrahmane1999@gmail.com>
2022-04-24 11:07:24 +0530

    my change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2 for webhook')
+  res.send('CICD App V6 for npm stage')
 })
 
 app.get('/status', (req, res) => {
